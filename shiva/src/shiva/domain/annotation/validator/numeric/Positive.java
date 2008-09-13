97286b92a6de05d22731d4a64a4458667e428fa8
paulovittor23@gmail.com <paulovittor23@gmail.com@4a264520-70ac-452e-abbb-38ac596b1b2e>
2008-09-13 22:47:11 +0000

    git-svn-id: svn://localhost/tmp/project-shiva/trunk@21 4a264520-70ac-452e-abbb-38ac596b1b2e

---
+++ shiva/src/shiva/domain/annotation/validator/numeric/Positive.java
@@ -0,0 +1,23 @@
+package shiva.domain.annotation.validator.numeric;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 
+ * 
+ * @author paulo
+ * 
+ * @description
+ *
+ *
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface Positive {
+	
+	
+
+}
