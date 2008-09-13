d59000cf34f3ab03aa560cd707b2ba0320cf72d2
paulovittor23@gmail.com <paulovittor23@gmail.com@4a264520-70ac-452e-abbb-38ac596b1b2e>
2008-09-13 22:01:46 +0000

    git-svn-id: svn://localhost/tmp/project-shiva/trunk@7 4a264520-70ac-452e-abbb-38ac596b1b2e

---
+++ shiva/src/shiva/domain/exception/UidAttributeNotFoundException.java
@@ -0,0 +1,43 @@
+package shiva.domain.exception;
+
+/**
+ * 
+ * @author Paulo Vitor
+ * @author Roberto Su
+ * 
+ * @description
+ * 
+ */
+@SuppressWarnings( "unchecked" )
+public class UidAttributeNotFoundException extends Exception {
+
+	/**
+     * 
+     */
+	private static final long serialVersionUID = 4748709829356716872L;
+
+	/**
+	 * 
+	 * 
+	 */
+	public UidAttributeNotFoundException() {
+		super();
+	}
+
+	/**
+	 * 
+	 * @param mensagem
+	 */
+	public UidAttributeNotFoundException( String mensagem ) {
+		super( mensagem );
+	}
+
+	/**
+	 * 
+	 * @param clazz
+	 */
+	public UidAttributeNotFoundException( Class clazz ) {
+		super( "The registered class \"" + clazz.getCanonicalName() + "\" must have an uid attribute." );
+	}
+
+}
